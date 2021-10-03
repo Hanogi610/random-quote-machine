276e668911205f7c487c071cfc20ab5c6bc9ca97
Hoang Pham <phh61002@gmail.com>
2021-10-03 20:54:37 +0700

    fetching so have to reload the page, not yet to find the solution

---
+++ index.js
@@ -0,0 +1,23 @@
+var currentQuote = "";
+var currentAuthor = "";
+function $(x) {
+  return document.getElementById(x);
+}
+async function getQuotes(){
+  const reponse = await fetch('quote.json');
+  const data = await reponse.json();
+  return data.quotes[Math.floor(Math.random()*data.quotes.length)];
+}
+
+function getQuote(){
+  getQuotes().then(key => {
+    $("author").innerHTML = key.author;
+    $("text").innerHTML = key.quote;
+  });
+  $("tweet-quote").setAttribute(
+    "href",
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+}
+$("new-quote").onclick = getQuote();
