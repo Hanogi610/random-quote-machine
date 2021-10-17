f56e14a0f159b4a86bceac7e8774b72cdd11af97
Hoang Pham <phh61002@gmail.com>
2021-10-17 16:14:10 +0700

    patch

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-var currentQuote = "";
-var currentAuthor = "";
-function $(x) {
-  return document.getElementById(x);
-}
-async function getQuotes(){
-  const reponse = await fetch('quote.json');
-  const data = await reponse.json();
-  return data.quotes[Math.floor(Math.random()*data.quotes.length)];
+const useState = React.useState;
+const useEffect = React.useEffect;
+
+function App(){
+  const [content, setContent] = useState({quote: 'Without music, life would be a mistake.', author: 'Friedrich Nietzsche'});
+  useEffect(()=>{
+    console.log(content)
+    document.getElementById("author").innerHTML = content.author;
+    document.getElementById("text").innerHTML = content.quote;
+    document.getElementById("tweet-quote").setAttribute(
+        "href",
+        "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+encodeURIComponent(
+          '"' + content.author + '" ' + content.quote)
+      );
+  },[content]);
+
+  function getQuote(){
+    fetch('./quote.json')
+    .then(response=>response.json())
+    .then(data=>{setContent(data.quotes[Math.floor(Math.random()*data.quotes.length)])});
+  }
+
+  return (
+    <div id="quote-box" className="quote-box shadow p-3 mb-5 bg-white rounded">
+      <div className="quote-text blockquote text-center">
+        <i className="fa fa-quote-left"> </i>
+        <span id="text" ></span>
+        <p id="author" className="blockquote-footer"></p>
+      </div>
+      <div className="d-flex justify-content-between">
+        <a
+          className="btn btn-outline-primary"
+          id="tweet-quote"
+          title="Tweet this quote!"
+          target="_top"
+        >
+          <i className="fa fa-twitter"></i>
+        </a>
+        <button id="new-quote" className="btn btn-outline-success" onClick={getQuote}>
+          <span>New Quote</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-function getQuote(){
-  getQuotes().then(key => {
-    $("author").innerHTML = key.author;
-    $("text").innerHTML = key.quote;
-    $("tweet-quote").setAttribute(
-      "href",
-      "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-        encodeURIComponent('"' + key.author + '" ' + key.quote)
-    );
-  });
-}
-$("new-quote").onclick = getQuote();
+ReactDOM.render(<App/>,document.getElementById("root"));
